1dc2f18cc51f262336d6c04ff7109fc13d169329
emopetrovIT <em.petrov88@gmail.com>
2022-07-21 17:00:31 +0300

    complete UI and fetch quotes

---
+++ quote-generator/script.js
@@ -0,0 +1,22 @@
+let apiQuotes = [];
+
+// Show New Quote
+function newQuote() {
+  // Pick a random quote from apiQuotes array
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+}
+
+// Get Quotes From API
+async function getQuotes() {
+  const apiUrl = 'https://type.fit/api/quotes';
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    console.log(apiQuotes);
+  } catch (error) {
+    alert(error);
+  }
+}
+
+// On Load
+getQuotes();
