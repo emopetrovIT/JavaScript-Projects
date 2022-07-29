e6a9c080894cc1bf7a1b27e0fd0820cbb86bfccf
emopetrovIT <em.petrov88@gmail.com>
2022-07-29 12:30:15 +0300

    Add helper function setAttributes()

--- infinite-scroll/script.js
+++ infinite-scroll/script.js
@@ -6,15 +6,25 @@ const count = 10;
 const apiKey = '6kcVUDDNn-Xn06YN795rn72QjrK-1RD6LkoZ5036GGA';
 const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
 
+function setAttributes(element, attributes) {
+  for (const key in attributes) {
+    element.setAttribute(key, attributes[key]);
+  }
+}
+
 function displayPhotos() {
   photosArray.forEach((photo) => {
     const item = document.createElement('a');
-    item.setAttribute('href', photo.links.html);
-    item.setAttribute('target', '_blank');
+    setAttributes(item, {
+      href: photo.links.html,
+      target: '_blank',
+    });
     const img = document.createElement('img');
-    img.setAttribute('src', photo.urls.regular);
-    img.setAttribute('alt', photo.alt_decription);
-    img.setAttribute('title', photo.alt_decription);
+    setAttributes(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description,
+    });
     item.appendChild(img);
     imageContainer.appendChild(item);
   });
