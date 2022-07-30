797b49649597989df7be7359352c6b0c25b7f77e
emopetrovIT <em.petrov88@gmail.com>
2022-07-30 14:24:32 +0300

    Finish Scrolling Functionality

--- infinite-scroll/script.js
+++ infinite-scroll/script.js
@@ -1,10 +1,24 @@
 const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
+
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 let photosArray = [];
 
-const count = 10;
+const count = 30;
 const apiKey = '6kcVUDDNn-Xn06YN795rn72QjrK-1RD6LkoZ5036GGA';
 const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
+
+function imageLoaded() {
+  imagesLoaded++;
+
+  if (imagesLoaded === totalImages) {
+    ready = true;
+    loader.hidden = true;
+    console.log(imagesLoaded);
+  }
+}
 
 function setAttributes(element, attributes) {
   for (const key in attributes) {
@@ -13,18 +27,24 @@ function setAttributes(element, attributes) {
 }
 
 function displayPhotos() {
+  imagesLoaded = 0;
+  totalImages = photosArray.length;
+
   photosArray.forEach((photo) => {
     const item = document.createElement('a');
     setAttributes(item, {
       href: photo.links.html,
       target: '_blank',
     });
+
     const img = document.createElement('img');
     setAttributes(img, {
       src: photo.urls.regular,
       alt: photo.alt_description,
       title: photo.alt_description,
     });
+
+    img.addEventListener('load', imageLoaded);
     item.appendChild(img);
     imageContainer.appendChild(item);
   });
@@ -39,5 +59,15 @@ async function getPhotos() {
     alert(error);
   }
 }
+
+window.addEventListener('scroll', () => {
+  if (
+    window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 &&
+    ready
+  ) {
+    ready = false;
+    getPhotos();
+  }
+});
 
 getPhotos();
